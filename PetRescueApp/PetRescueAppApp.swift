ac996379d5a51c90f31dfed765df3d806d7734aa
Zytron1228 <dastelljes@outlook.com>
2021-11-09 14:36:12 -0600

    Initial Commit

---
+++ PetRescueApp/PetRescueAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetRescueAppApp.swift
+//  PetRescueApp
+//
+//  Created by PMStudent on 11/9/21.
+//
+
+import SwiftUI
+
+@main
+struct PetRescueAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
