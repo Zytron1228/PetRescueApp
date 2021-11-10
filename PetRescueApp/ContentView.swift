d5105a4c882feb44faf286e84b71c8f707fdd4ec
Zytron1228 <dastelljes@outlook.com>
2021-11-10 14:44:06 -0600

    E
    not done

--- PetRescueApp/ContentView.swift
+++ PetRescueApp/ContentView.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, lost pet!")
-            .font(Font .custom("Montserrat-Regular", size: 17))
+        VStack {
+            Spacer()
+            TextEditor(text: .constant("messagEk!?iohqtAa")).font(Font .custom("galactic-simple", size: 33))
+            
+        Text("Aa Bb Cc Dd Ee Ff Gg Hh Ii Jj Kk Ll Mm Nn Oo Pp Qq Rr Ss Tt Uu Vv Ww Xx Yy Zz .!?$ 1234567890*")
+            .font(.custom("Montserrat-ExtraLight.ttf", size: 32))
             .padding()
+            
+            Text("Aa Bb Cc Dd Ee Ff Gg Hh Ii Jj Kk Ll Mm Nn Oo Pp Qq Rr Ss Tt Uu Vv Ww Xx Yy Zz .!?$ 1234567890*")
+                .padding()
+                .font(Font .custom("JoeMama-Obamaa", size: 32))
+            
+            Text("Aa Bb Cc Dd Ee Ff Gg Hh Ii Jj Kk Ll Mm Nn Oo Pp Qq Rr Ss Tt Uu Vv Ww Xx Yy Zz .!?$ 1234567890*")
+                .font(.largeTitle)
+                .padding()
+        }
     }
 }
 
