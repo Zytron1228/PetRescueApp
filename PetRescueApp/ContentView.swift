2d50fa6290913b293a054f51f1e8c27f26301ad7
Zytron1228 <dastelljes@outlook.com>
2021-11-16 12:18:05 -0600

    e
    e

--- PetRescueApp/ContentView.swift
+++ PetRescueApp/ContentView.swift
@@ -469,6 +469,6 @@ struct ContentView_Previews: PreviewProvider {
 //breed : shepherd bully lab  mixed *
 //size: small medium large x large *
 // gender : m/f/unsure *
-// age: 
+// age:
 // other: blue collar *
 // submit button
