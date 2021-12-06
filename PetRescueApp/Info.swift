451ae8220db258fc14b7143e216ab87cf23daef6
Zytron1228 <dastelljes@outlook.com>
2021-12-06 13:04:32 -0600

    Filled in help&info.
    Also Merged Info and Help pages.

--- PetRescueApp/Info.swift
+++ PetRescueApp/Info.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct Info: View {
     var body: some View {
-        Text("Info WIP")
+        Text("This is the help & information page for this app. \n \nThis app let users submit a form for a lost pet or for a pet they found and are trying to find it's owner. The user can then search for pets that are similar in their description that have also been submitted on this app. \nPress the side bar button in the top-right to navigate to different pages. If you have lost a pet, go to submit a form, then click Lost a pet, and fill out a form. If you have found a missing pet and want to find it's owner, Click Found a pet instead. You can then procede to search lost or found pets and the app will help filter pets by similar details in the forms. ")
+            .padding()
     }
 }
 
